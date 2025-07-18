089988f5ce53c2088ca08adf8791aa86ef8b78bf
samhitha176 <srisamhitharayudu@gmail.com>
2025-07-18 12:05:25 +0530

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,13 @@
+#include "Queue.h"
+int main() {
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
+    q.dequeue();
+    q.display();
+    cout<<q.peek();
+    
+    return 0;
+}
